ff4943adbae5b80f0a036318d201d967df0fb9bd
Davide Garberi <dade.garberi@gmail.com>
2023-11-08 21:36:02 +0100

    gmscompat: Use new info
    Change-Id: I3cb0c55d28249b73ecc53be83bed030304c782d9
    Signed-off-by: Kneba <abenkenary3@gmail.com>

--- core/java/com/android/internal/gmscompat/AttestationHooks.java
+++ core/java/com/android/internal/gmscompat/AttestationHooks.java
@@ -109,11 +109,11 @@ public final class AttestationHooks {
 
     private static void spoofBuildGms() {
         // Alter model name and fingerprint to avoid hardware attestation enforcement
-        setBuildField("FINGERPRINT", "asus/WW_Phone/ASUS_X00HD_4:7.1.1/NMF26F/14.2016.1801.372-20180119:user/release-keys");
-        setBuildField("PRODUCT", "WW_Phone");
-        setBuildField("DEVICE", "ASUS_X00HD_4");
-        setBuildField("MODEL", "ASUS_X00HD");
-        setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N_MR1);
+        setBuildField("DEVICE", "bullhead");
+        setBuildField("FINGERPRINT", "google/bullhead/bullhead:8.0.0/OPR6.170623.013/4283548:user/release-keys");
+        setBuildField("MODEL", "Nexus 5X");
+        setBuildField("PRODUCT", "bullhead");
+        setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N);
     }
 
     public static void initApplicationBeforeOnCreate(Application app) {
